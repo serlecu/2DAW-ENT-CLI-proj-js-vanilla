bbdd79cf21c7615a2c0b267750c8c91ec43c894e
Sergio Lecuona <serlecu@hotmail.com>
2025-12-02 15:54:48 +0100

    pagina3 foundation

---
+++ js/vistas/main_p3.js
@@ -0,0 +1,17 @@
+// import { renderFormPregunta } from "./formPregunta.js";
+// import { renderListaPreguntas } from "./listaPreguntas.js";
+
+function main() {
+    console.log("Pantalla 3");
+    console.log(sessionStorage);
+
+    const contForm = document.getElementById("container_form");
+    contForm.innerHTML = "<p>Formulario Placeholder</p>";
+    // renderFormPregunta(contForm);
+
+    const contLista = document.getElementById("container_lista");
+    contLista.innerHTML = "<p>Cargando preguntas...</p>";
+    // renderListaPreguntas(contLista);
+}
+
+document.addEventListener('DOMContentLoaded', main);
